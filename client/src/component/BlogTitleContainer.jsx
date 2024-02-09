c3281aa2cf13ace5ac70ad367b534b6f721a5857
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-02-09 13:08:00 +0530

    Update BlogTitleContainer.jsx

--- client/src/component/BlogTitleContainer.jsx
+++ client/src/component/BlogTitleContainer.jsx
@@ -5,6 +5,9 @@ import { Link } from 'react-router-dom'
 const BlogTitleContainer = ({blogs}) => {
     const url = "https://blogv3server.onrender.com";
     // {`http://localhost:5000/${row.image.slice(14)}`} 
+     const padd={
+        'paddingRight':'0px'
+    }
     return (
         <>  
         <Link className='cus_link' to={`/blog/${blogs._id}`}>
@@ -14,18 +17,18 @@ const BlogTitleContainer = ({blogs}) => {
                     <div className="author_title hev">{blogs.author[0].name}</div>
                     <div className="author_date hev">{`${blogs.author[0].time?blogs.author[0].time.slice(0,10):''}`}</div>
                 </div>
-                <div className='blog_detail'>
-                <div className="bdp2">
-                        <img src={`${url}/${blogs.image?.slice(14)}`} className='img-fluid' alt="" />
+                <div className='blog_detail row'>
+                        <div className="bdp2 col-lg-3 col-md-2 col-3" style={padd}>
+                            <img src={`${url}/${blogs.image.slice(14)}`} className='img-fluid' alt="" />
+                        </div>
+                        <div className='bdp1 col-lg-8 col-md-9 col-9'>
+                            <h2 className='blog_title text-start'>{blogs.post_title}</h2>
+                            <h3 className='blog_para text-start'>
+                                {blogs.post_para.length > 250 ?
+                                    `${blogs.post_para.substring(0, 250)}...` : blogs.post_para
+                                }</h3>
+                        </div>
                     </div>
-                    <div className='bdp1'>
-                        <h2 className='blog_title text-start'>{blogs.post_title}</h2>
-                        <h3 className='blog_para text-start'>
-                        {blogs.post_para.length > 250 ?
-                            `${blogs.post_para.substring(0, 250)}...` : blogs.post_para
-                        }</h3>
-                    </div>                    
-                </div>
                 <div className='blog_tag'>
                     <div className="bt">
                         <h5 className='me-2'> <span class="rounded-pill cus_bg2 fw-normal badge text-dark">{blogs.category}</span> </h5>
