de512f3233f82858479748f3c02d11906553010f
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-02-09 19:46:24 +0530

    Update Privateroute.jsx

--- client/src/component/Privateroute.jsx
+++ client/src/component/Privateroute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const Privateroute = () => {
     const auth = localStorage.getItem('user');
-    return auth?<Outlet />:<Navigate to="adminlogin"/>
+    return auth?<Outlet />:<Navigate to="/adminlogin"/>
 }
 
 export default Privateroute
