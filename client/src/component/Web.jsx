3376b25afee96a8f289ac976aea779cd3a6fdc4c
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2024-02-14 17:24:36 +0530

    Update Web.jsx

--- client/src/component/Web.jsx
+++ client/src/component/Web.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
-import Divider from './Divider'
-
+// import Divider from './Divider'
+const Divider = React.lazy(()=>  import("./Divider"));
+// import Skeleton from 'react-loading-skeleton'
 const Web = () => {
   return (
     <>
         <Header/>
+        <Suspense fallback={<div><h1>oading</h1></div>}>
         <Divider/>
+                </Suspense>   
+          
     </>
   )
 }
